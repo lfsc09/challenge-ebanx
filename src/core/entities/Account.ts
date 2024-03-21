ff7c85857aee8f31e1ffa074933ae0b8f7027097
lfsc09 <lfsc09@gmail.com>
2024-03-21 01:56:48 -0300

    feat: added withdraw check for enough balance

--- src/core/entities/Account.ts
+++ src/core/entities/Account.ts
@@ -1,24 +1,26 @@
 export class Account {
 	constructor(private account_id: string, private balance: number) {
-        if (!account_id.match(/[\d]+/)) throw new Error("invalid account_id");
-        if (!balance.toString().match(/[\d]+/) || balance < 0) throw new Error("invalid amount");
-    }
+		if (!account_id.match(/[\d]+/)) throw new Error('invalid account_id');
+		if (!balance.toString().match(/[\d]+/) || balance < 0) throw new Error('invalid amount');
+	}
 
-    deposit(amount: number) {
-        if (!amount.toString().match(/[\d]+/) || amount <= 0) throw new Error('invalid amount');
-        this.balance += amount;
-    }
-    
-    withdraw(amount: number) {
-        if (!amount.toString().match(/[\d]+/) || amount <= 0) throw new Error('invalid amount');
-        this.balance -= amount;
-    }
+	deposit(amount: number) {
+		if (!amount.toString().match(/[\d]+/) || amount <= 0) throw new Error('invalid amount');
+		this.balance += amount;
+	}
 
-    getAccountId(): string {
-        return this.account_id;
-    }
+	withdraw(amount: number) {
+		if (!amount.toString().match(/[\d]+/) || amount <= 0) throw new Error('invalid amount');
+		if (this.balance - amount < 0) throw new Error('not enough balance');
 
-    getBalance(): number {
-        return this.balance;
-    }
+		this.balance -= amount;
+	}
+
+	getAccountId(): string {
+		return this.account_id;
+	}
+
+	getBalance(): number {
+		return this.balance;
+	}
 }
--- test/unit/Account.test.ts
+++ test/unit/Account.test.ts
@@ -4,31 +4,48 @@ describe('Account', () => {
 	test('Create Account normal values', () => {
 		try {
 			const newAccount = new Account('100', 10);
-            expect(newAccount.getBalance()).toBe(10);
+			expect(newAccount.getBalance()).toBe(10);
 		} catch (err: any) {}
 	});
 
-    test('Create Account wrong id', () => {
+	test('Create Account wrong id', () => {
 		try {
-			const newAccount = new Account("das sdasf", 10);
+			const newAccount = new Account('das sdasf', 10);
 		} catch (err: any) {
-            expect(err.message).toBe('invalid account_id');
-        }
+			expect(err.message).toBe('invalid account_id');
+		}
 	});
 
-    test('Create Account negative amount', () => {
+	test('Create Account negative amount', () => {
 		try {
-			const newAccount = new Account("100", -10);
+			const newAccount = new Account('100', -10);
 		} catch (err: any) {
-            expect(err.message).toBe('invalid amount');
-        }
+			expect(err.message).toBe('invalid amount');
+		}
 	});
 
-    test('Create Account string amount', () => {
+	test('Create Account string amount', () => {
 		try {
-			const newAccount = new Account("100", <number><unknown>'12asdasf');
+			const newAccount = new Account('100', <number>(<unknown>'12asdasf'));
 		} catch (err: any) {
-            expect(err.message).toBe('invalid amount');
-        }
+			expect(err.message).toBe('invalid amount');
+		}
+	});
+
+	test('Withdraw ok', () => {
+		try {
+			const newAccount = new Account('100', 10);
+			newAccount.withdraw(10);
+			expect(newAccount.getBalance).toBe(0);
+		} catch (err: any) {}
+	});
+
+	test('Withdraw fail, not enough balance', () => {
+		try {
+			const newAccount = new Account('100', 10);
+			newAccount.withdraw(15);
+		} catch (err: any) {
+			expect(err.message).toBe('not enough balance');
+		}
 	});
 });
