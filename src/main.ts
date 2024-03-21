2ef5383a5f205f1c7e42c1616551eaf095235192
lfsc09 <lfsc09@gmail.com>
2024-03-21 01:46:52 -0300

    update: added custom request to hold the repository and during execution

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,34 @@
 import bodyParser from 'body-parser';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import http from 'http';
 import { handleApiConfigCors, handleApiErrors } from './infra/middlewares/ApiMiddlewares';
+import { ApiRepositoryMemory } from './infra/repositories/memory/ApiRepository';
+import ApiRoutes from './infra/routes/ApiRoutes';
+
+export interface CustomRequest extends Request {
+    [key: string]: any;
+}
 
 const serverInit = async (listeningPort: number = 5000) => {
     const server = express();
+    let accountManager = new Map();
+    let apiRepositoryMemory = new ApiRepositoryMemory(accountManager);
 
 	/********************************
 	 * CONFIGURE HEADERS AND REQUEST
 	 ********************************/
 	server.use(handleApiConfigCors);
 	server.use(bodyParser.json());
+    server.use((request: CustomRequest, response: Response, next: NextFunction) => {
+        request['apiRepositoryMemory'] = apiRepositoryMemory;
+        next();
+    });
 
+    /*********
+     * ROUTES
+     *********/
+    server.use(ApiRoutes);
+    
 	/********************
 	 * GRAB ASYNC ERRORS
 	 ********************/
