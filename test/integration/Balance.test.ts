2b8ac42a4f6eeddd1e4dde948c3ab862ec77e908
lfsc09 <lfsc09@gmail.com>
2024-03-21 09:30:33 -0300

    feat: new Balance and Transfer tests

---
+++ test/integration/Balance.test.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+describe('Withdraw Usecases', () => {
+	beforeAll(async () => {
+		await axios.post('http://127.0.0.1:5000/reset');
+		await axios.post('http://127.0.0.1:5000/event', {
+			type: 'deposit',
+			destination: '100',
+			amount: 50,
+		});
+	});
+
+	test('Check Balance existent account', async () => {
+		try {
+			await axios.post('http://127.0.0.1:5000/event', {
+				type: 'withdraw',
+				destination: '100',
+				amount: 20,
+			});
+			let response = await axios.get('http://127.0.0.1:5000/balance?account_id=100');
+			expect(response.status).toBe(200);
+			expect(response.data).toEqual(30);
+		} catch (err: any) {}
+	});
+
+	test('Check Balance non existent account', async () => {
+		try {
+			let response = await axios.get('http://127.0.0.1:5000/balance?account_id=500');
+		} catch (err: any) {
+			expect(err.response.status).toBe(404);
+			expect(err.response.data).toBe(0);
+		}
+	});
+});
---
+++ test/integration/Transfer.test.ts
@@ -0,0 +1,86 @@
+import axios from 'axios';
+
+describe('Transfer Usecases', () => {
+	beforeAll(async () => {
+        await axios.post('http://127.0.0.1:5000/reset');
+		await axios.post('http://127.0.0.1:5000/event', {
+			type: 'deposit',
+			destination: '100',
+			amount: 10,
+		});
+        await axios.post('http://127.0.0.1:5000/event', {
+			type: 'deposit',
+			destination: '200',
+			amount: 50,
+		});
+	});
+
+	test('Invalid amount', async () => {
+		try {
+			let response = await axios.post('http://127.0.0.1:5000/event', {
+				type: 'transfer',
+				origin: '200',
+				destination: '100',
+				amount: -10,
+			});
+		} catch (err: any) {
+			expect(err.response.status).toBe(500);
+			expect(err.response.data).toBe('invalid amount');
+		}
+	});
+
+	test('Transfer from existent accounts', async () => {
+		try {
+			let response = await axios.post('http://127.0.0.1:5000/event', {
+				type: 'transfer',
+				origin: '200',
+				destination: '100',
+				amount: 15,
+			});
+			expect(response.status).toBe(200);
+			expect(response.data).toEqual({ origin: { id: '200', balance: 35 }, destination: { id: '100', balance: 25 } });
+		} catch (err: any) {}
+	});
+
+    test('Transfer from existent accounts - not enough balance', async () => {
+		try {
+			let response = await axios.post('http://127.0.0.1:5000/event', {
+				type: 'transfer',
+				origin: '200',
+				destination: '100',
+				amount: 40,
+			});
+		} catch (err: any) {
+            expect(err.response.status).toBe(500);
+			expect(err.response.data).toBe('not enough balance');
+        }
+	});
+
+    test('Transfer from non-existent account', async () => {
+		try {
+			let response = await axios.post('http://127.0.0.1:5000/event', {
+				type: 'transfer',
+				origin: '500',
+				destination: '200',
+				amount: 10,
+			});
+		} catch (err: any) {
+            expect(err.response.status).toBe(404);
+			expect(err.response.data).toBe(0);
+        }
+	});
+
+	test('Transfer to non-existent account', async () => {
+		try {
+			let response = await axios.post('http://127.0.0.1:5000/event', {
+				type: 'transfer',
+				origin: '100',
+				destination: '500',
+				amount: 15,
+			});
+		} catch (err: any) {
+			expect(err.response.status).toBe(404);
+			expect(err.response.data).toBe(0);
+		}
+	});
+});
